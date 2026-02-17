b318312f96da0d516bac0bbe0eb56d424caa832d
SebastianEsc05 <sebastianescram01@gmail.com>
2026-02-17 13:49:07 -0700

    Practica 5

---
+++ NetBeansProjects/Practicas/NebulaMusicWebAplication/src/main/java/repositories/UsuarioDB.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package repositories;
+
+import java.util.HashMap;
+import java.util.Map;
+import models.Usuario;
+
+/**
+ *
+ * @author sebas
+ */
+public class UsuarioDB {
+    public static Map<String, Usuario> usuarios = new HashMap<>();
+    
+    public static void registrar(Usuario usuario){
+        usuarios.put(usuario.getCorreo(),usuario);
+    }
+    
+    public static boolean autenticar(String correo, String contrasenia){
+        if(!usuarios.containsKey(correo)){
+            return false;
+        }
+        return usuarios.get(correo).getContrasenia().equals(contrasenia);
+    }
+    
+    public static boolean existeCorreo(String correo){
+        return usuarios.containsKey(correo);
+    }
+    
+    
+}
